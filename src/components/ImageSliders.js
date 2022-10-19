9a4f99b65c618341d3b496650e76e9de78d54458
Akil Grant <akil.grant.93@gmail.com>
2022-10-19 14:52:27 -0400

    animation added to nametag

--- src/components/ImageSliders.js
+++ src/components/ImageSliders.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-
+import './anim.css'
 class ImageSlider extends Component {
   constructor(props) {
     super(props);
@@ -8,6 +8,39 @@ class ImageSlider extends Component {
       paused: false,
     };
   }
+
+
+
+  /**
+ * letteranimation.js
+ * https://www.cssanimatio.io
+ * Created and maintained by: Pavel
+ * Find me at: https://www.linkedin.com/in/yesiamrocks/
+ * Email: hello@cssanimation.io
+ * Github: https://github.com/yesiamrocks/cssanimation.io
+ * Title: CSS Animation Library for Developers and Ninjas
+ * Copyright (c) 2017 Pavel
+ * License: cssanimattion is licensed under the MIT license
+ **/
+
+animateSequence() {
+  var a = document.getElementsByClassName('sequence');
+  for (var i = 0; i < a.length; i++) {
+      var $this = a[i];
+      var letter = $this.innerHTML;
+      letter = letter.trim();
+      var str = '';
+      var delay = 100;
+      for (let l = 0; l < letter.length; l++) {
+          if (letter[l] != ' ') {
+              str += '<span style="animation-delay:' + delay + 'ms; -moz-animation-delay:' + delay + 'ms; -webkit-animation-delay:' + delay + 'ms; ">' + letter[l] + '</span>';
+              delay += 150;
+          } else
+              str += letter[l];
+      }
+      $this.innerHTML = str;
+  }
+}
 
   componentDidMount() {
     setInterval(() => {
@@ -19,6 +52,9 @@ class ImageSlider extends Component {
         this.setState({ currentSlide: newSlide });
       }
     }, 3000);
+
+
+    this.animateSequence();
   }
 
   setCurrentSlide = (index) => {
@@ -28,8 +64,8 @@ class ImageSlider extends Component {
   render() {
     return (
         <div className="min-w-screen flex relative">
-            <div className="frame">
-              <span className="absolute z-10 mt-[25vh] left-1/4 text-white w-1/2 top-1/3 text-center font-extrabold text-twoxl uppercase select-none cursor-pointer">Akil Grant</span>
+            <div className="cssanimation">
+              <span className="absolute z-10 mt-[25vh] left-1/4 text-white w-1/2 top-1/3 text-center font-extrabold text-twoxl uppercase select-none cursor-pointer sequence">Akil Grant</span>
             </div>
             {this.props.slides.map((slide, index) => {
               return (
