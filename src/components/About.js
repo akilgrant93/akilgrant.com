6cc1f03a37156016043141942a5a2452370a624a
Akil Grant <akil.grant.93@gmail.com>
2022-11-10 11:20:56 -0500

    lottie animations added succesfully

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Lottie from "lottie-react";
+import meAnimated from "../myself.json";
 
 export default function About() {
   return (
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+        <div className="lg:flex-grow md:w-1/2 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="text-xl mb-4 text-forest-green font-black">
             Hey, I'm Akil!
             <br className="hidden lg:inline-block" />I'm a UX Designer and Art Director.
@@ -15,11 +17,12 @@ export default function About() {
 
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img
-            className="object-cover object-center rounded"
+          {/* <img
+            className=""
             alt="hero"
             src="./coding.svg"
-          />
+          /> */}
+          <Lottie className="object-cover object-center rounded" animationData={meAnimated} />;
         </div>
       </div>
     </section>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,23 +1,29 @@
 import React, {useState} from "react";
-import SkillAnimation from "./SkillAnimation";
+import Lottie from "lottie-react";
 import './skills.css'
+import userExperience from "../user-experience.json";
+import artDirection from "../art-direction.json";
+import motionGraphics from "../motion-graphics.json"
+import illustration from "../illustration.json"
+
 
 export default function Skills() {
 	const [selected, setSelected] = useState(null)
 
+	const skillsArr = ['User Experience', 'Illustration', 'Motion Graphics', 'Art Direction']
+
+	const lottieArr = [userExperience, illustration, motionGraphics, artDirection]
+
 	const handleClick = (e) => {
-		console.log(e.target.value)
-		setSelected(e.target)
+		setSelected(e.target.value.slice(-1))
 
 	}
 
 
-	const skillsArr = ['User Experience', 'Illustration', 'Motion Graphics', 'Art Direction']
-
   return (
     <section id="">
       <div className="container mt-32 mx-auto">
-        <div className="flex items-center lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+        <div className="flex items-center lg:w-4/5 sm:mx-auto justify-evenly sm:mb-2 -mx-2">
         <div className="list w-64">
 		<div className="head">
 			<div className="title">Skills & Tech</div>
@@ -37,9 +43,9 @@ export default function Skills() {
 				</div>
 				{selected === null
 				? <div>
-						<img className='max-w-2xl ml-20' src='standard-01.png'/>
+						<img className='max-w-2xl' src='standard-01.png'/>
 					</div>
-					: ''}
+					: <Lottie className="max-w-2xl" animationData={lottieArr[parseInt(selected)-1]} /> }
         </div>
       </div>
     </section>
