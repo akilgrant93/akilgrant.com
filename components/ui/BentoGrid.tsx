a142419bec5242a75b4a53a7e989d834fc5645bf
Akil Grant <akilgrant@Akils-MacBook-Pro.local>
2025-09-02 16:55:26 -0400

    reversed orientation of second bento box row

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -100,7 +100,7 @@ export const BentoGridItem = ({
           )}
         </div>
         <div
-          className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"
+          className={`absolute right-0 -bottom-5 ${id === 4 && "w-full opacity-80"
             } `}
         >
           {spareImg && (
@@ -130,7 +130,7 @@ export const BentoGridItem = ({
           )}
         >
           {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
-          <div className={`font-sans font-extralight md:max-w-60 ${id === 5 && 'tracking-[.25em]'} md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10`}>
+          <div className={`font-sans font-extralight md:max-w-60 ${id === 4 && 'tracking-[.25em]'} md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10`}>
             {description}
           </div>
           {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
@@ -140,7 +140,7 @@ export const BentoGridItem = ({
           >
             {title}
           </div>
-          {id === 4 && (
+          {id === 5 && (
             <Slideshow id={2}/>
           )}
 
--- data/index.ts
+++ data/index.ts
@@ -11,7 +11,7 @@ export const gridItems = [
     },
     {
         title:'Design enthusiast with a passion for plants and people', 
-        titleClassName:'justify-center md:justify-start lg:justify-center', 
+        titleClassName:'justify-end lg:justify-center', 
         className:'md:col-span-3 md:row-span-2', 
         img:'', 
         spareImg:'', 
@@ -30,34 +30,34 @@ export const gridItems = [
         id:3
     },
     {
-        title:"I'm really down to earth", 
-        titleClassName:'justify-center', 
-        className:'lg:col-span-2 md:col-span-3 md:row-span-1', 
-        img:'', 
-        spareImg:'', 
-        imgClassName:'', 
-        description: '', 
-        id:4
-    },
-    {
-        title:'Building Urban Farms across New York City', 
-        titleClassName:'justify-center md:justify-start lg:justify-center max-w-80', 
-        className:'md:col-span-3 md:row-span-2', 
-        img:'/54688826826_1645c70476_k.jpg', 
-        spareImg:'', 
-        imgClassName:'top-10 ml-72 rounded-lg opacity-80', 
-        description: 'MY LATEST PROJECT', 
-        id:5
-    },
-    {
-        title:"Let's Build!", 
-        titleClassName:'justify-center md:max-w-full max-w-60 text-center', 
-        className:'lg:col-span-2 md:col-span-3 md:row-span-1', 
-        img:'', 
-        spareImg:'', 
-        description: '', 
-        id:6
-    },
+        id: 4,
+        title:"Building Urban Farms across New York City", 
+        description: "MY LATEST PROJECT",
+        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+        imgClassName: "top-10 ml-72 rounded-lg opacity-80",
+        titleClassName: "justify-center max-w-80",
+        img: "/54688826826_1645c70476_k.jpg",
+        spareImg: "",
+      },
+      {
+        id: 5,
+        title: "I'm really down to earth",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "",
+        spareImg: "",
+      },
+      {
+        id: 6,
+        title: "Lets Build!",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-center",
+        img: "",
+        spareImg: "",
+      },
 ]
 
 export const images = ['illustration05.png', 'logo04.png','clip_2.gif', 'https://images.squarespace-cdn.com/content/v1/5363e3d1e4b0b6dbd37bcdd6/e021804c-58af-4a8c-88ef-01b451ff618a/02.jpg?format=2500w','engraving.gif', 'cici_01.png','shoelala_01.8d3a7d03.png','illu02.png', 'illu01.png', 'illu02.png', 'https://images.squarespace-cdn.com/content/v1/5363e3d1e4b0b6dbd37bcdd6/dd864302-2414-4436-8ec9-fa9a5e75addc/04.png?format=2500w', 'clip_5.gif','https://images.squarespace-cdn.com/content/v1/5363e3d1e4b0b6dbd37bcdd6/6c91b27f-a30c-4298-b3a9-512581ad4a9a/03.jpeg?format=2500w','Silenced.png', 'logo01.png', 'https://images.squarespace-cdn.com/content/v1/5363e3d1e4b0b6dbd37bcdd6/336faf2a-b6bd-4f37-a6a8-8481083f0226/01.jpg?format=2500w', 'growr_mockup_sq_sm.c5aa7d2b.jpg', 'shoelala_01.8d3a7d03.png', 'bando_01.png', 'blkcat_02.png', 'image-asset11.3bf6c368.png','clip_5.gif', 'logo06.png', 'bando_01.png', 'logo07.png','', 'logo08.png', 'logo09.png', 'logo10.png'];
